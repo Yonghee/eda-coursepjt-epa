7e5d7ef46e20dbff07a930892e875f3390cf4385
Yonghee <yhlee73@gmail.com>
2014-05-15 10:43:44 +0900

    Plot5 Completed

---
+++ plot4.R
@@ -0,0 +1,12 @@
+nei <- readRDS("./data/summarySCC_PM25.rds")
+scc <- readRDS("./data/Source_Classification_Code.rds")
+
+sccLists <- scc[grepl("coal",scc$EI.Sector, ignore.case=T),]$SCC
+sumData <- nei[nei$SCC %in% sccLists,]
+sumData1 <- aggregate(Emissions ~ year, sumData, sum)
+par(mfrow=c(1,1))
+plot(sumData1$year, sumData1$Emissions,pch=19,xlab="YEAR", ylab="Total Emissions", main="Emissions from coal combustion-related sources- USA")
+lines(sumData1$year, sumData1$Emissions, col="blue")
+dev.copy(png,"./plot4.png",width=480, height=480)
+dev.off()
+
